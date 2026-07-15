28fa3fc8014eb376bc8af690212a0d9104061242
marcosjoojm-bit <marcosjoo.jm@gmail.com>
2026-07-15 09:13:51 -0300

    Initial commit

---
+++ src/main/java/com/projetoIntegrador/Studia/StudiaApplication.java
@@ -0,0 +1,13 @@
+package com.projetoIntegrador.Studia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoIntegrador/Studia/StudiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoIntegrador.Studia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
